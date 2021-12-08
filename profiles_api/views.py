bb03020b1b1dc052e7c514bff218cff299d0b61d
Saurabh <spaulmailme@gmail.com>
2021-12-08 18:03:42 +0530

    Adding APIView with get method

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,1 +1,17 @@
 # Create your views here.
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class HelloApiView(APIView):
+    """Test API View"""
+
+    def get(self, request, format=None):
+        """Returns a API View features"""
+        an_apiview = [
+            'Uses HTTP methods as functions (get, post, put, delete)',
+            'Is similer to a traditional Django View',
+            'Gives you the most over the applicaion logic',
+            'Is mapped manually to the URLs'
+        ]
+        return Response({'message': 'Hello World', 'an_apiview': an_apiview})
