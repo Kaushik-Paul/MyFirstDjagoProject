0a8517d83bc3d7d9fc63aaaf95ebf34f22120a34
Saurabh <spaulmailme@gmail.com>
2021-12-08 19:22:40 +0530

    Adding APIView with post,patch,delete method

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,10 +1,14 @@
 # Create your views here.
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
+from profiles_api import serializers
 
 
 class HelloApiView(APIView):
     """Test API View"""
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """Returns a API View features"""
@@ -15,3 +19,29 @@ class HelloApiView(APIView):
             'Is mapped manually to the URLs'
         ]
         return Response({'message': 'Hello World', 'an_apiview': an_apiview})
+
+    def post(self, request, format=None):
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f"Hello {name}"
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def put(self, request, format=None, pk=None):
+        """Handling updating an object"""
+        return Response({'message': 'PUT Request'})
+
+    def patch(self, request, format=None, pk=None):
+        """Handling the partial update to an object"""
+        return Response({'message': "PATCH Request"}, status=status.HTTP_201_CREATED)
+
+    def delete(self, request, format=None, pk=None):
+        """Delete an object"""
+        return Response({'message': "DELETE Request"}, status=status.HTTP_202_ACCEPTED)
