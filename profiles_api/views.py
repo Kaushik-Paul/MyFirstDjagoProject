4cf430c3dcf0c68b99f32600b7247fb476aafb22
Saurabh <spaulmailme@gmail.com>
2021-12-10 12:53:33 +0530

    Added Search Functionality and permission to User Profile

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -104,8 +104,13 @@ class HelloViewSet(ViewSet):
 class UserProfileViewSet(viewsets.ModelViewSet):
     """Handling creating and updating profiles"""
     serializer_class = serializers.UserProfileSerializer
+
     queryset = models.UserProfile.objects.all()
+
     authentication_classes = (TokenAuthentication,)
+
     permission_classes = (permission.UpdateOwnProfile,)
+
     filter_backends = (filters.SearchFilter,)
+
     search_fields = ('name', 'email',)
